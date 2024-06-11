892751f1775c203f36b1b5b8081cc71f70c369ec
Nishant Popli <nishantpopli1234@gmail.com>
2024-06-11 16:56:33 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://trusty-bedbug-6.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
